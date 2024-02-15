36efe44f06150082e53caba1a88d6cddefc24479
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-02-15 10:54:19 -0300

    Adicionando a função de apagar e editar uma tarefa

--- Jorge Teixeira/todo/urls.py
+++ Jorge Teixeira/todo/urls.py
@@ -4,4 +4,5 @@ from todo import views
 
 urlpatterns = [
     path("", views.todo_list),
+    path("<int:pk>", views.todo_detail_and_todo_delete),
 ]
--- Jorge Teixeira/todo/views.py
+++ Jorge Teixeira/todo/views.py
@@ -18,3 +18,26 @@ def todo_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def todo_detail_and_todo_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
