ed29148edfc28509d691d581e5208f9cff28e57f
Paulo Henrif <paulohenriquef931@gmail.com>
2024-02-17 14:01:39 -0300

    .

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuButton = document.getElementById('burguer');
-    const navList = document.getElementById('nav-list');
-  
-    mobileMenuButton.addEventListener('click', function () {
-      navList.classList.toggle('show');
-    });
+  const mobileMenuButton = document.getElementById('burguer');
+  const navList = document.getElementById('nav-list');
+
+  mobileMenuButton.addEventListener('click', function () {
+    navList.classList.toggle('show');
   });
-  
+});
